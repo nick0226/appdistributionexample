13a4a251449cead0b40d86585b48156a542bb8d5
Nikita <kit1datasvit@gmail.com>
2025-04-14 11:50:44 +0300

    -release #2

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -56,8 +56,8 @@ android {
         applicationId = "com.appdistributionexample"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
     }
     packaging {
         resources {
